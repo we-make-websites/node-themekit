92962ce572d82cb6b78452de8af91772d0df21b5
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-12-13 09:29:34 +0100

    Merge pull request #985 from Shopify/release-1.3.1
    Release 1.3.1
    Conflicts:
    	Makefile
    	choco/themekit.nuspec
    	choco/tools/chocolateyinstall.ps1
    	dev.yml
    	docs/scripts/install.py
    	scripts/install.py
    	src/release/release.go
    	src/release/release_test.go

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = $env:ChocolateyPackageName
+$file           = "$($toolsDir)\theme.exe"
+$version        = "v1.3.1"
+$url            = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-386/theme.exe"
+$url64          = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-amd64/theme.exe"
+$checksum       = '20292407e05210e43dd2a4ee0e660321'
+$checksum64     = '8541545ada519c5a9f780b7eb05b5c02'
+$validExitCodes = @(0)
+
+Get-ChocolateyWebFile `
+  -PackageName $packageName `
+  -FileFullPath $file `
+  -Url "$url" `
+  -Checksum $checksum `
+  -ChecksumType "md5" `
+  -Url64bit "$url64" `
+  -Checksum64 $checksum64 `
+  -ChecksumType64 "md5"
